61c67a9e08dd6722eb2772a244fc60aefff13728
jessica <jessica_02_06@outlook.com>
2022-09-08 09:15:57 -0300

    entrega mercado

--- products.js
+++ products.js
@@ -1,56 +1,56 @@
 const products = [
   {
     title: "Uva Crimson",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Frutas",
-    image: undefined,
+    image:"./img/products/no-img.svg",
     imageDescription: "",
   },
   {
     title: "Banana",
-    price: 5.69,
+    price:"R$ 5.69",
     category: "Frutas",
     image: "./img/products/product_2.svg",
     imageDescription: "",
   },
   {
     title: "Mamão",
-    price: 4.99,
+    price: "R$ 4.99",
     category: "Frutas",
     image: "./img/products/product_3.svg",
     imageDescription: "",
   },
   {
     title: "Maçã",
-    price: 9.2,
+    price: "R$ 9.20",
     category: "Frutas",
     image: "./img/products/product_4.svg",
     imageDescription: "",
   },
   {
     title: "Refrigerante",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Bebidas",
-    image: undefined,
+    image: "./img/products/no-img.svg",
     imageDescription: "",
   },
   {
     title: "Vinho",
-    price: 8.99,
+    price:"R$8.99",
     category: "Bebidas",
     image: "./img/products/product_6.svg",
     imageDescription: "",
   },
   {
     title: "Água Tônica",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Bebidas",
-    image: undefined,
+    image: "./img/products/no-img.svg",
     imageDescription: "",
   },
   {
     title: "Água de coco",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Bebidas",
     image: "./img/products/product_8.svg",
     imageDescription: "",
@@ -58,7 +58,7 @@ const products = [
 
   {
     title: "Sabonete",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Higiene",
     image: "./img/products/product_9.svg",
     imageDescription: "",
@@ -66,7 +66,7 @@ const products = [
 
   {
     title: "Detergente",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Higiene",
     image: "./img/products/product_10.svg",
     imageDescription: "",
@@ -74,7 +74,7 @@ const products = [
 
   {
     title: "Limpa superfícies",
-    price: 8.99,
+    price: "R$ 8.99",
     category: "Higiene",
     image: "./img/products/product_11.svg",
     imageDescription: "",
@@ -82,9 +82,85 @@ const products = [
 
   {
     title: "Lustra Móveis",
-    price: 8.99,
+    price:"R$ 8.99",
     category: "Higiene",
-    image: undefined,
+    image: "./img/products/no-img.svg",
     imageDescription: "",
   },
 ];
+
+const listaSecaoFrutas = document.querySelector(".products-content-fruits")
+
+const listaSecaoDrinks = document.querySelector(".products-content-drinks")
+
+const listaSecaoHigiene = document.querySelector(".products-content-hygiene")
+
+function listaDeProdutoFruit (lista, referenciaHtml){
+  for(let i = 0; i < lista.length; i++){
+    let produto = lista[i]
+    if(produto.category == "Frutas"){
+      let produtoCriado = criarProdutos(produto)
+      referenciaHtml.appendChild(produtoCriado)
+    }
+}
+}
+listaDeProdutoFruit(products, listaSecaoFrutas)
+
+function listaDeProdutoDrink (lista, referenciaHtml){
+  for(let i = 0; i < lista.length; i++){
+    let produto = lista[i]
+    if(produto.category == "Bebidas"){
+      let produtoCriado = criarProdutos(produto)
+      referenciaHtml.appendChild(produtoCriado)
+    }
+}
+}
+listaDeProdutoDrink(products, listaSecaoDrinks)
+
+function listaDeProdutoHigiene (lista, referenciaHtml){
+  for(let i = 0; i < lista.length; i++){
+    let produto = lista[i]
+    if(produto.category == "Higiene"){
+      let produtoCriado = criarProdutos(produto)
+      referenciaHtml.appendChild(produtoCriado)
+    }
+}
+}
+listaDeProdutoHigiene(products, listaSecaoHigiene)
+
+function criarProdutos (produto){
+
+  let imagem          = produto.image
+  let nome            = produto.title
+  let secao           = produto.category
+  let valor           = produto.price
+  let descricaoImagem = produto.imageDescription
+  
+  let tagLi     = document.createElement("li")
+  let tagImg    = document.createElement("img")
+  let tagMain   = document.createElement("main")
+  let tagH1     = document.createElement("h1")
+  let tagH5     = document.createElement("h5")
+  let tagStrong = document.createElement("strong")
+  
+  tagLi.classList.add("product")
+  tagImg.classList.add("product-img")
+  tagMain.classList.add("product-main")
+  tagH1.classList.add("product-title")
+  tagH5.classList.add("product-category")
+  tagStrong.classList.add("product-price")
+  
+  tagImg.src      = imagem
+  tagImg.alt      = descricaoImagem
+  tagH1.innerText = nome
+  tagH5.innerText = secao
+  tagStrong.innerHTML = valor
+  
+  tagLi.appendChild(tagImg)
+  tagLi.appendChild(tagMain)
+  tagMain.appendChild(tagH1)
+  tagMain.appendChild(tagH5)
+  tagMain.appendChild(tagStrong)
+
+  return tagLi
+}
